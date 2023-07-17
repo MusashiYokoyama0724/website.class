1a92d76c48c1334b2140e788b20e9c2832660bd6
MusashiYokoyama0724 <139727912+MusashiYokoyama0724@users.noreply.github.com>
2023-07-16 19:53:16 -0700

    Add files via upload

---
+++ login_managers.php
@@ -0,0 +1,12 @@
+
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>ログイン成功</title>
+  <link rel="stylesheet" href="css/test.css" type="text/css">
+ </head>
+ <body>
+    <h1>ログイン</h1>
+    <h2>管理者としてログイン成功しました</h2>
+ </body>
+</html> 
